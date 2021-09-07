8ed8905a8fa19c39bdac48df6ef8baf948808558
NaufalAmajid <naufalamajid@gmail.com>
2021-09-07 18:20:02 +0700

    first commit

---
+++ Assets/BallControl.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    //RigidBody 2D Bola
+    private Rigidbody2D rigidbody2D;
+
+    //Besar Gaya awal untuk mendorong bola
+    public float xInitialForce;
+    public float yInitialForce;
+
+    //MODIFIKASI SCRIPT
+    private Vector2 trajectoryOrigin;
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        trajectoryOrigin = transform.position;
+    }
+    public Vector2 TrajectoryOrigin
+    {
+        get { return trajectoryOrigin;  }
+    }
+
+    //SCRIPT
+    void ResetBall()
+    {
+
+        transform.position = Vector2.zero;
+
+        rigidbody2D.velocity = Vector2.zero;
+
+    }
+
+    void PushBall()
+    {
+
+        //float yRandomInitialForce = Random.Range(-yInitialForce, yInitialForce);
+
+        float RandomDirection = Random.Range(0, 2);
+
+        if(RandomDirection < 1.0f)
+        {
+            rigidbody2D.AddForce(new Vector2(-xInitialForce, yInitialForce));
+        }
+        else
+        {
+            rigidbody2D.AddForce(new Vector2(xInitialForce, yInitialForce));
+        }
+
+    }
+
+    void RestartGame()
+    {
+        ResetBall();
+
+        Invoke("PushBall", 2);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rigidbody2D = GetComponent<Rigidbody2D>();
+
+        RestartGame();
+
+        //MODIFIKASI SCRIPT
+        trajectoryOrigin = transform.position;
+    }
+
+}
---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    //Tombol untuk gerak keatas
+    public KeyCode upButton = KeyCode.W;
+
+    //Tombol untuk gerak kebawah
+    public KeyCode downButton = KeyCode.S;
+
+    //Kecepatan gerak
+    public float speed = 10.0f;
+
+    //batas atas dan bawah game scene (batas bawah dengan ( - ))
+    public float yBoundary = 9.0f;
+
+    //rigidbody 2D this raket
+    private Rigidbody2D rigidbody2D;
+
+    //skor
+    public int score;
+
+    //MODIFIKASI SCRIPT
+    private ContactPoint2D lastContactPoint;
+    public ContactPoint2D LastContactPoint
+    {
+        get { return lastContactPoint;  }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name.Equals("Ball"))
+        {
+            lastContactPoint = collision.GetContact(0);
+        }    
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rigidbody2D = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        //UNTUK MENGGERAKKAN RAKET
+        Vector2 velocity = rigidbody2D.velocity;
+        if (Input.GetKey(upButton))
+        {
+            velocity.y = speed;
+        }else if (Input.GetKey(downButton))
+        {
+            velocity.y = -speed;
+        }else
+        {
+            velocity.y = 0.0f;
+        }
+        rigidbody2D.velocity = velocity;
+
+        //BATASAN GERAK RAKET
+        Vector2 position = transform.position;
+        if(position.y > yBoundary)
+        {
+            position.y = yBoundary;
+        }else if (position.y < - yBoundary)
+        {
+            position.y = -yBoundary;
+        }
+        transform.position = position;
+
+        //MEMANIPULASI SCORE
+        
+    }
+
+    //MEMANIPULASI SCORE
+    public void IncrementScore()
+    {
+        score++;
+    }
+    public void ResetScore()
+    {
+        score = 0;
+    }
+    public int Score
+    {
+        get { return score; }
+    }
+}
